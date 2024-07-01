ceab65d96dc6e038f4bc4cae09c8860bb6792fc7
jloucksdev <julialoucks@gmail.com>
2024-07-01 20:55:24 +0100

    amended styles to bring back colours!

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-import { data } from './data/css-file-list.js';
+import { data } from "./data/css-file-list.js";
 
 function populateDropdown() {
-  const dropdownContent = document.getElementById('theme-dropdown-content');
-  
-  // Clear existing content
-  dropdownContent.innerHTML = '';
+  const dropdownContent = document.getElementById("theme-dropdown-content");
 
-  data.forEach(theme => {
-    const link = document.createElement('a');
-    link.href = '#';
-    link.textContent = theme.replace('.css', ''); // Display the theme name without '.css'
+  // Clear existing content
+  dropdownContent.innerHTML = "";
+
+  data.forEach((theme) => {
+    const link = document.createElement("a");
+    link.href = "#";
+    link.textContent = theme.replace(".css", ""); // Display the theme name without '.css'
     link.onclick = (event) => {
       event.preventDefault();
       changeStylesheet(theme);
@@ -19,14 +19,16 @@ function populateDropdown() {
 }
 
 function changeStylesheet(fileName) {
-  const stylesheet = document.getElementById('stylesheet');
-  stylesheet.href = `styles/${fileName}`; 
+  const stylesheet = document.getElementById("stylesheet");
+  stylesheet.href = `styles/${fileName}`;
   populateDropdown();
 }
 
-document.addEventListener('DOMContentLoaded', populateDropdown);
+document.addEventListener("DOMContentLoaded", populateDropdown);
 
-document.getElementById('theme-dropdown-button').addEventListener('click', () => {
-  const dropdownContent = document.getElementById('theme-dropdown-content');
-  dropdownContent.classList.toggle('show');
-});
+document
+  .getElementById("theme-dropdown-button")
+  .addEventListener("click", () => {
+    const dropdownContent = document.getElementById("theme-dropdown-content");
+    dropdownContent.classList.toggle("show");
+  });
